06647b50066578a390b28479972d0a3bd4b6f262
Liselle Logan <liselle.logan@gmail.com>
2020-09-25 00:08:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,8 +8,6 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    BRANDS.uniq
-    #binding.pry
   end
 
   def cobble
@@ -17,4 +15,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+BRANDS.uniq
 end
